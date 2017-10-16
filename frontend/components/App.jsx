eb0ac5def5b630a85798db1047ce94217634097b
Max <maxkoerbaecher@gmail.com>
2017-10-16 23:38:00 +0200

    add websocket echo, need fix

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -14,11 +14,41 @@ class App extends Component{
             connected: false
           };
     }
+    componentDidMount(){
+        let ws = this.ws = new WebSocket('ws://echo.websocket.org');
+        ws.onmessage = this.message.bin(this);
+        ws.onopen = this.open.bin(this);
+        ws.onclose = this.close.bind(this);
+    }
+    message(e){
+        const event = JSON.parse(e.data);
+        if(event.name === 'channel add'){
+            this.newChannel(event.data);
+        }
+    }
+    open(){
+        this.setState({connected: true});
+    }
+    close(){
+        this.setState({connected: false});
+    }
+    newChannel(channel){
+        let {channels} = this.state;
+        channels.push(channel);
+        this.setState({channels});
+    }
     addChannel(name){
         let {channels} = this.state;
-        channels.push({id: channels.length, name});
-        this.setState({channels});
+       
         //TODO: Send to server
+        let msg = {
+            name: 'channel add',
+            data: {
+                id: channels.length,
+                name
+            }
+        }
+        this.ws.send(JSON.stringify(msg))
     }
     setChannel(activeChannel){
         this.setState({activeChannel});
